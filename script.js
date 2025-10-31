51d367abacdc20b18d76c1da77f56a98cd8b9a33
sagar kumar <94098920+Sagar-Kumar01@users.noreply.github.com>
2025-10-31 12:40:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your code here
+let n = [2,1,2];
+
+let map = new Map();
+
+n.forEach(v => {
+	if(!map.has(v)){
+		map.set(v,1);
+	}else{
+		map.set(v,map.get(v)+1);
+	}
+})
+
+for(let [key,value] of map){
+	if(value > Math.floor(n.length/2)){
+		return key;
+	}
+}
+
